299890f6c5c0235e47b784f57c6562d852311c0b
devngho <yjh135908@gmail.com>
2022-04-02 15:51:55 +0900

    🚀not registered fix

--- registry.go
+++ registry.go
@@ -1,7 +1,20 @@
 package mcmplgo
 
-var tasks = make(map[string]TaskType, 0)
-var requests = make(map[string]RequestType, 0)
-var events = make(map[string]EventType, 0)
+var tasks = map[string]TaskType{
+	"OnEnable":  OnEnable,
+	"EventTask": EventTask,
+}
+var requests = map[string]RequestType{
+	"Log":           Log,
+	"RegisterEvent": RegisterEvent,
+	"RegisterTask":  RegisterTask,
+}
+var events = map[string]EventType{
+	"BlockBreakEvent":    BlockBreakEvent,
+	"BlockBurnEvent":     BlockBurnEvent,
+	"BlockCanBuildEvent": BlockCanBuildEvent,
+	"PlayerJoinEvent":    PlayerJoinEvent,
+	"PlayerQuitEvent":    PlayerQuitEvent,
+}
 var listeners = make(map[EventType][]EventListener, 0)
 var taskListeners = make(map[TaskType][]TaskListener, 0)
